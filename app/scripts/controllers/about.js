9ffa63b50c982eb719cd4950dba16c2b4e8fe235
Roger Eveland <reveland@illinois.edu>
2017-03-08 18:27:44 -0600

    Initial Commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name finalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the finalApp
+ */
+angular.module('finalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
